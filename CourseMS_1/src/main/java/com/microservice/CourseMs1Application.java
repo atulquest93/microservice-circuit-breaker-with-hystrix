c384d55ec60e3b67d8876a6c705db8d1d1a40697
atulquest93 <atulquest93@gmail.com>
2017-10-08 00:44:11 +0530

    Eureka , Microservice , Hystrix

---
+++ CourseMS_1/src/main/java/com/microservice/CourseMs1Application.java
@@ -0,0 +1,14 @@
+package com.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class CourseMs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMs1Application.class, args);
+	}
+}
